5d2104b95ae60496286173aa5fe2ecff7868e983
Pankratov <pnrf@yandex.ru>
2022-04-01 00:50:36 +0300

    feat: add section about and swiper slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,39 @@
 /** swiper-about (для секции: Несколько слов о нас (about))
  * --- НАЧАЛО КОДА ----
 */
-import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
 
-let swiper = new Swiper(".swiper-about", {
-  slidesPerView: 2,
-  spaceBetween: 32,
-  slidesPerGroup: 2,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32
+    }
+  },
+
   loop: true,
-  loopFillGroupWithBlank: true,
+
+  grabCursor: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
+  }
 });
 /** swiper-about (для секции Несколько слов о нас (about))
  * --- КОНЕЦ КОДА --- */
