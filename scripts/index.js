b2dcf5b61473de8998afc4fd70e0ee29ae5779df
Pankratov <pnrf@yandex.ru>
2022-04-03 19:13:43 +0300

    feat: try to make drop-down menu

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,8 @@ const swiper = new Swiper(".swiper", {
 
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    type: 'bullets',
+    clickable: true
   },
 
   navigation: {
@@ -36,4 +37,146 @@ const swiper = new Swiper(".swiper", {
   }
 });
 /** swiper-about (для секции Несколько слов о нас (about))
+ * --- КОНЕЦ КОДА --- */
+
+
+
+/** карточки (для секции: Проекты (projects))
+ * --- НАЧАЛО КОДА ----
+*/
+
+/** для темплейта */
+const itemTemplate = document.querySelector('#template-projects-cards'); // ссылка на темплейт
+const listElement = document.querySelector('.projects__cards-list'); // ссылка на родителя (куда вставить темплейт)
+
+/** для меню (projects__menu) */
+const navProjects = document.querySelector('.projects__menu');
+
+
+/** функция: сгенерировать карточку из темлейта:
+ * 1) клонировать темплейт из html в DOM;
+ * 2) наполнить темплейт содержимым: название места, ссылка на картинку, alt к картинке;
+ * 3) установить слушатели: на картинку, на кнопку лайк/дизлайк, на кнопку удаления карточки (корзинку);
+ *
+ * на вход функция получает объект, поэтому обращаемся к свойствам объекта можно через точку: item.name и item.link,
+ * через ключ-переменную: item['name'] и item['link'] либо воспользоваться деструктуризацией: const createCard = ({name, link}) => {};
+ * данная функция возвращает сгенерированную карточку;
+ * для отрисовки карточки на странице используется функция renderCard();
+*/
+const createCard = ({logo, text, bgrImg}) => {
+  // клонировал темлейт из html в DOM
+  const itemElement = itemTemplate.content.cloneNode(true);
+  // ввел переменную внутри функции
+  const cardLogoElm = itemElement.querySelector('.projects__card-logo'); // ссылка на картинку в карточке
+  // наполнил темплейт содержимым:
+  itemElement.querySelector('.projects__card-text').textContent = text;
+  cardLogoElm.src = logo;
+  cardLogoElm.alt = `${logo}. Логотип`;
+  //установил картинку в background
+  itemElement.querySelector('.projects__card').style.background = `center / cover no-repeat url(${bgrImg}), #009FAC`;
+
+  return itemElement;
+};
+
+/** функция: отрисовать карточку на странице (вставить в разметку):
+ * данная функция переиспользуется для отрисовки исходного массива и для добавления новой карточки;
+ * на вход функция получает 2 (два) строчных значения (текст и ссылку), которые пользователь ввел в форму;
+ * эти значения присваиваются ключам-переменным (name, link);
+ * затем ключи-переменные объединяем и в качестве (!)объекта передаем в функцию createCard;
+*/
+
+const renderCard = (logo, text, bgrImg) => {
+  listElement.appendChild(createCard({logo, text, bgrImg}));
+};
+
+/** перебрать исходный массив, и отрисовать карточки по порядку
+ * карточки должны выводиться на страницу в ограниченном количестве, в зависимости от разрешения экрана,
+ * для этого вначале делим массив на несколько массивов методом slice
+*/
+
+/**Алгоритм:
+ * 1) подсчитать общее количество объектов в массиве.
+ * 2) поделить количество объектов на число доступных ячеек на странице (в зависимости от разрешения экрана) = число страниц.
+ * 3) разбить исходный массив на несколько массивов количеством, равным числу страниц.
+ * 4) каждой странице присвоить свой массив (№ страницы -- это индекс мелкого массива).
+ * 5) отрисовать тот массив, номер страницы которого активен.
+ */
+
+/*
+let b;
+
+const ArrLength = () => {
+if (window.innerWidth <= 768) {
+  b = projectsCards.length / 4;
+} else if (window.innerWidth <= 1024) {
+  b = projectsCards.length / 6;
+} else {
+  b = projectsCards.length / 8;
+};
+
+return Math.ceil(b);// число страниц = количество элементов в массиве / число карточек на странице
+// столько же цифорок нужно отрисовать в пагинации
+};
+
+const chunkArray = b => {
+  ArrLength ();
+
+  let i, newArray = [];
+  for (i = 0; i < projectsCards.length; i += b) {
+    newArray.push(projectsCards.slice(i, i + b));
+  }
+  console.log(newArray);
+};
+*/
+
+/** Изначально отприсовать все карточки на странице */
+projectsCards.forEach(item => renderCard(item.logo, item.textMain, item.bgrImgage));
+
+/** Открыть/закрыть меню бамбургера */
+const projectsBurgerElm = document.querySelector('.projects__burger');
+const projectsBurgerNavBtn = projectsBurgerElm.querySelector('.projects__burger-nav-button');
+const projectsBurgerNavList = projectsBurgerElm.querySelector('.projects__burger-nav-list');
+
+const toggleBurgerNav = () => {
+  projectsBurgerNavList.classList.toggle('projects__burger-nav-list_opened');
+};
+
+projectsBurgerNavBtn.addEventListener('click', toggleBurgerNav);
+
+/** Фильтрация карточек по id */
+const renderFilteredCards = (evt) => {
+  // 1) переключить активную кнопку
+  navProjects.querySelectorAll('.projects__menu-button').forEach(item => {
+    if (item.classList.contains('projects__menu-button_active')) {
+      item.classList.remove('projects__menu-button_active');
+    };
+    return;
+  });
+
+  evt.target.classList.add('projects__menu-button_active');
+
+
+  // 2) удалить все карточки со страницы
+  listElement.querySelectorAll('.projects__card').forEach(item => {
+    item.remove();
+  });
+
+
+  // 3) отрисовать на странице отфильтрованные карточки
+  projectsCards.forEach(item => {
+    // console.log(item, item.id);
+
+    if (item.id === evt.target.getAttribute('id') || evt.target.getAttribute('id') === 'allProjects') {
+      renderCard(item.logo, item.textMain, item.bgrImgage);
+    };
+  });
+};
+
+navProjects.querySelectorAll('.projects__menu-button').forEach(evt => {
+  evt.addEventListener('click', renderFilteredCards);
+});
+
+
+
+/** карточки (для секции: Проекты (projects))
  * --- КОНЕЦ КОДА --- */
---
+++ scripts/projectCards.js
@@ -0,0 +1,122 @@
+const projectsCards = [
+  {
+    id: 'govProjects',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'govProjects',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'govProjects',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'specProjects',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/gazprom-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_gazprom.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+  {
+    id: 'inProgress',
+    logo: './images/Projects/sberbank-logo.svg',
+    textMain: 'Направление включает в себя исследования и разработки технологий МО и КТ.',
+    textAdded: 'Модули для внедрения в предметные сетевые программы магистратуры с элементами МО и КТ, или программы ДПО.',
+    bgrImgage: './images/Projects/project_snerbank.png'
+  },
+];
+
